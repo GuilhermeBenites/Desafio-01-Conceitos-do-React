2e3b7e064bde08e54ef8e35c06a1bd7c1b920ca2
Guilherme Benites <guilherme.rebello91@gmail.com>
2021-12-10 05:19:08 -0400

    Solução do desafio 1 - Reactjs

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,31 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle){
+      const actualTasks = [...tasks];
+      actualTasks.push({id: Math.random(), title: newTaskTitle, isComplete: false});
+
+      setTasks(actualTasks);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const actualTasks = [...tasks];
+
+    let changedTaskIndex = actualTasks.findIndex((t => t.id === id));
+
+    if(changedTaskIndex !== -1){
+      actualTasks[changedTaskIndex].isComplete = !actualTasks[changedTaskIndex].isComplete;
+
+      setTasks(actualTasks);
+    }
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(t => t.id != id);
+
+    setTasks(newTasks);
   }
 
   return (
@@ -32,9 +48,9 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
@@ -50,7 +66,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -66,7 +82,7 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
